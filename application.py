1e90c49f071bdef1282fc8de2967aaee48496f12
BalaKarthikeya19 <balakarthikeya19@gmail.com>
2023-07-03 20:17:20 +0530

    removed bottle

--- application.py
+++ application.py
@@ -1,7 +1,6 @@
 from flask import Flask,request,render_template
 import numpy as np
 import pandas as pd
-from bottle import debug
 
 from sklearn.preprocessing import StandardScaler
 from predict_pipeline import CustomData,PredictPipeline
